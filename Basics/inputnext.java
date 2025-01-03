b3c313f8e1c34865139ef455f388b01db6436ae9
gitcrusher <aayushguptasoni@gmail.com>
2025-01-03 20:02:39 +0530

    coomit

---
+++ Basics/inputnext.java
@@ -0,0 +1,10 @@
+package Basics;
+import java.util.Scanner;
+public class inputnext {
+    public static void main (String[] args){
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+    System.out.println(str);
+    sc.close();
+    }
+}
