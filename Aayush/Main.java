31374b6e6f96455b422704ccf9e42c1a90b44333
gitcrusher <aayushguptasoni@gmail.com>
2025-01-03 09:59:33 +0530

    commit

--- Aayush/Main.java
+++ Aayush/Main.java
@@ -3,6 +3,11 @@ package Aayush;
 public class Main {
     public static void main(String[] args) {
         byte age =  34;
+        char name = 'A';
+        float f1 = 5.6f;
+        long l1 = 212334535246356l;
+        double d1 = 4.98d;
+        boolean a = true;
         System.out.println(age);
     }
 }
