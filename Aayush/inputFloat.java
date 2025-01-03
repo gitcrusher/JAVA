d91ded15674504825549551ae9d594d7251f59ef
gitcrusher <aayushguptasoni@gmail.com>
2025-01-03 18:10:26 +0530

    commit

--- Aayush/inputFloat.java
+++ Aayush/inputFloat.java
@@ -1,7 +1,11 @@
 package Aayush;
-import 
+import java.util.Scanner;
 public class inputFloat {
     public static void main(String[] args){
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a float number");
+        float a = sc.nextFloat();
+        System.out.println(a);
+        sc.close();
     }
 }
