65e7cf0fc6f4fd7b8a8681fb398833b149c184fb
gitcrusher <aayushguptasoni@gmail.com>
2025-01-03 18:57:36 +0530

    commit

--- Basics/inputHasnext.java
+++ Basics/inputHasnext.java
@@ -1,6 +1,6 @@
 package Basics;
 import java.util.Scanner;
-public class inputHasnext {
+public class inputHasNEXT {
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
         boolean b1 = sc.hasNextInt();
