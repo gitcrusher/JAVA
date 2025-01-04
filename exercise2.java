18ad6cb4e6e89b1ba5e52a7b9d5ceeb07c93351d
gitcrusher <aayushguptasoni@gmail.com>
2025-01-04 11:08:27 +0530

    commit

--- exercise2.java
+++
@@ -1,7 +0,0 @@
-public class exercise2 {
-    public static void main (String[] args){
-        char grade = 'B';
-        grade = (char)(grade + 2);
-        System.out.println("after typecasting: "+ grade);
-    }
-}
