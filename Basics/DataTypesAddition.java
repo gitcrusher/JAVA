801710d9a785f03ac88bc3c5f320b84c8a0ac0b6
gitcrusher <aayushguptasoni@gmail.com>
2025-01-04 10:44:28 +0530

    commit

---
+++ Basics/DataTypesAddition.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class DataTypesAddition {
+    public static void main (String[] args){
+        byte b1 = 10;
+        int b2 = 20;
+        byte sum = (byte)(b1 + b2);
+        System.out.println("Sum of byte values: " + sum);
+    }
+}
