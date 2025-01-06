3d8d5c0e4780495de66fc0699a54d599a53af037
gitcrusher <aayushguptasoni@gmail.com>
2025-01-06 10:40:31 +0530

    commit

---
+++ Strings/fprintformat.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class fprintformat {
+    public static void main(String[] args) {
+        int a = 2567;
+        float b = 3.32256434f;
+        System.out.printf("The folloing formating tells you that a is %d and b is %f",a,b);
+    }
+    
+}
