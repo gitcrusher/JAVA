8f53d66a4d85832e29a38e2bf6a6707f6f1dbec5
gitcrusher <aayushguptasoni@gmail.com>
2025-01-06 10:42:47 +0530

    commit

--- Strings/fprintformat.java
+++ Strings/fprintformat.java
@@ -4,7 +4,8 @@ public class fprintformat {
     public static void main(String[] args) {
         int a = 2567;
         float b = 3.32256434f;
-        System.out.printf("The folloing formating tells you that a is %d and b is %f",a,b);
+        //System.out.printf("The folloing formating tells you that a is %d and b is %f",a,b);
+        System.out.format("The folloing formating tells you that a is %d and b is %f",a,b);
     }
     
 }
