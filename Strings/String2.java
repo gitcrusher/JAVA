6c32a0515cee8b1ec2ca3309b9903f14452f4d10
gitcrusher <aayushguptasoni@gmail.com>
2025-01-06 10:56:49 +0530

    commit

--- Strings/String2.java
+++ Strings/String2.java
@@ -4,7 +4,7 @@ public class String2 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        System.out.println(str.toLowerCase());
+        System.out.println(str.length());
         sc.close();
     }
 }
