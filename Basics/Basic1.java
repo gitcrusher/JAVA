93d9d36a909342875a7700eb489c51e47b63ca8b
gitcrusher <aayushguptasoni@gmail.com>
2025-01-20 11:47:09 +0530

    coomit

--- Basics/Basic1.java
+++ Basics/Basic1.java
@@ -7,8 +7,12 @@ public class Basic1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long start = sc.nextLong();
+        int i = sc.nextInt();
+        sc.nextLine();
+        String name = sc.nextLine();
         System.out.println("the data is long which is "+start);
-        System.out.println("the data is too long");
+        System.out.println("the data is in int "+ i);
+        System.out.println(name);
         sc.close();
     }
 }
