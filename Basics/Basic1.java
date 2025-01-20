c808b1f60256d352cba166a79ee5fc458fa62f54
gitcrusher <aayushguptasoni@gmail.com>
2025-01-20 11:26:09 +0530

    commit

---
+++ Basics/Basic1.java
@@ -0,0 +1,13 @@
+package Basics;
+import java.util.Scanner;
+
+
+
+public class Basic1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long start = sc.nextLong();
+        System.out.println("the data is long which is "+start);
+        sc.close();
+    }
+}
