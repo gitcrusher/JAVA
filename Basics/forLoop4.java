43261d0e87fe10c3a1e11bbf2744a44f3bd162fe
gitcrusher <aayushguptasoni@gmail.com>
2025-02-03 10:45:31 +0530

    commit

---
+++ Basics/forLoop4.java
@@ -0,0 +1,9 @@
+package Basics;
+
+public class forLoop4 {
+    public static void main(String[] args) {
+        for(char a:"abcdefghijklmnopqrstuvwxyzABCDEF".toCharArray()){
+            System.out.println(a);
+        }
+    }
+}
