6afe841aa4e1bb87b1f2c5bce4f55ce39bb0cbc3
gitcrusher <aayushguptasoni@gmail.com>
2025-02-03 10:26:19 +0530

    commit

--- Basics/forLoop.java
+++ Basics/forLoop.java
@@ -1,5 +1,10 @@
 package Basics;
 
 public class forLoop {
-    
+    public static void main(String[] args) {
+        int i = 0 ;
+        for(System.out.println("Bye");i<=10;i++) {
+            System.out.println("Hello!");
+        }
+    }
 }
