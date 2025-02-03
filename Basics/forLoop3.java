123474eb7b0cd3da3637777716c48b71e03aea12
gitcrusher <aayushguptasoni@gmail.com>
2025-02-03 10:32:18 +0530

    commit

---
+++ Basics/forLoop3.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class forLoop3 {
+    public static void main(String[] args) {
+        int i = 0;
+        for(char a ='A'; i <= 'E'; i++){
+            System.out.println(a);
+        }
+    }
+}
