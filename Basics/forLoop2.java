2b6be19dcce1fd43725bd19d0e1bca2cd90ef3cc
gitcrusher <aayushguptasoni@gmail.com>
2025-02-03 10:28:26 +0530

    commit

---
+++ Basics/forLoop2.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class forLoop2 {
+    public static void main(String[] args) {
+        int i = 0 ;
+        for(System.out.println("Hwllo");i<=10;i++,System.out.println("world")){
+            System.out.println("bye world");
+        }
+    }
+}
