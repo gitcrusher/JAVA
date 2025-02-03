d8e8fd0adca8e04ccb3e9755327bc6e1d4ad0c04
gitcrusher <aayushguptasoni@gmail.com>
2025-02-03 11:12:45 +0530

    commit

---
+++ Basics/forloop5.java
@@ -0,0 +1,12 @@
+package Basics;
+
+
+
+public class forloop5 {
+    public static void main(String[] args) {
+        for(int i = 0 ; i < 10 ; i++){
+            int j = 20 ;
+            System.out.println(j+i);
+        }
+    }
+}
