89bee3e610ff36354adbfb3205c70918653d4f2a
gitcrusher <aayushguptasoni@gmail.com>
2025-02-14 11:58:51 +0530

    commit

--- classes/classInheritance.java
+++ classes/classInheritance.java
@@ -14,4 +14,4 @@ public class classInheritance {
         A obj = new A(50, 60);
         A obj1 = new A(50, 60,70);
     }
-}
+}//if void was used then the java would understand it as a regular function 
