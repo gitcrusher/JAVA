e72a24fc91d91108bf33e1f286f3d5b110425c5e
gitcrusher <aayushguptasoni@gmail.com>
2025-02-14 11:13:45 +0530

    commit

--- Enum/enumConstructor.java
+++ Enum/enumConstructor.java
@@ -6,15 +6,25 @@ enum Month{
     Month(int day){
         totalDay = day;
     }
-    public int getTotalDay() {
-        return totalDay;
-    }
+    
 }
 
 public class enumConstructor {
     public static void main(String[] args) {
         for(Month m : Month.values()) 
-        System.out.println(m +" "+ m.getTotalDay());
+        switch (m) {
+            case Jan:
+                System.out.println(m.totalDay );
+                break;
+            case Feb:
+                System.out.println(m.totalDay);
+                break;
+            case Mar:
+                System.out.println(m.totalDay);
+                break;
+            default:
+                break;
+        }
     }
 
 }
