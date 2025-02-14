416552d12c2fcf5bd0428e4242ff79170ce2b915
gitcrusher <aayushguptasoni@gmail.com>
2025-02-14 10:51:52 +0530

    commit

--- WrapperClass/Autoboxing.java
+++ WrapperClass/Autoboxing.java
@@ -4,7 +4,7 @@ public class Autoboxing {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Integer num = sc.nextInt();//wrapper class
-        int a = 10  ;
+        
         System.out.println(num);//autoboxing
         System.out.println(num.intValue());//manual unboxing
         sc.close();
