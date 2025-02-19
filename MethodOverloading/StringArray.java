30a2b126fc1780742c71d0195207a958f80c2b64
gitcrusher <aayushguptasoni@gmail.com>
2025-02-19 16:39:01 +0530

    commit

---
+++ MethodOverloading/StringArray.java
@@ -0,0 +1,24 @@
+package MethodOverloading;
+
+public class StringArray {
+    public static void main(String[] args) {
+        String a = "Hello";
+        String b = new String("Bye");
+        System.out.println(a + " "+b);    
+        char c[] = {'c', 'b', 'g', 'h', 'o', 'p'}; // Corrected 'char' declaration
+        String cc = new String(c, 0, 6); // Creates a substring from index 1 of length 2
+        System.out.println(cc); // Output: "bg"
+
+        byte[] bytes = {65, 66, 67, 68}; // ASCII values for "ABCD"
+        String byteString = new String(bytes);
+        System.out.println(byteString); // Output: "ABCD"
+
+        char[] chars = {'X', 'Y', 'Z'};
+        String charString = new String(chars);
+        System.out.println(charString); 
+
+        String x = new String(bytes);
+        String e = new String(chars);
+        System.out.println(x+e);
+    }
+}
