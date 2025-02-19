0370bb8fb8e1addfbb7cf9cc28ff9e7b2b0f8e0b
gitcrusher <aayushguptasoni@gmail.com>
2025-02-19 16:50:36 +0530

    commit

---
+++ Strings/CharString.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class CharString {
+    public static void main (String[] args){
+        String str = "Hello, world!";
+
+        char a = str.charAt(5);
+
+        System.out.println(a); // H
+        System.out.println(str.length());
+    }
+}
