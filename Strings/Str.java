864d7db0558b27974d60889a25fc7a164b5bd5ff
gitcrusher <aayushguptasoni@gmail.com>
2025-02-19 16:58:14 +0530

    commit

---
+++ Strings/Str.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class Str {
+    public static void main(String[] args){
+        String a = "Hello";
+        String b = "World";
+        String c = a.concat("blah blah");
+        System.out.println(c+b);
+    }
+
+}
