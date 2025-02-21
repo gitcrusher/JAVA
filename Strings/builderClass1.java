091641472d3b73609862a6769e76ddf5a79b3878
gitcrusher <aayushguptasoni@gmail.com>
2025-02-21 10:52:26 +0530

    commit

---
+++ Strings/builderClass1.java
@@ -0,0 +1,12 @@
+package Strings;
+
+
+public class builderClass1 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Aayush");
+        sb.append("aayushsoni ");
+        System.out.println(sb.length());
+        System.out.println(sb.capacity());
+    }
+}
