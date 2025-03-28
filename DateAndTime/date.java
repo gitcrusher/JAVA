36c5467c42058243ad6ef3a3de8a94ba882ed389
gitcrusher <aayushguptasoni@gmail.com>
2025-03-28 10:30:24 +0530

    commit

--- DateAndTime/date.java
+++ DateAndTime/date.java
@@ -6,24 +6,13 @@ import java.util.*;
 
 public class date {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Ask the user for date input
-        System.out.print("Enter date in format (dd-MM-yyyy): ");
-        String userInput = scanner.nextLine();
-
-        // Define the date format
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-
-        // Parse the date
-        try {
-            LocalDate userDate = LocalDate.parse(userInput, formatter);
-            System.out.println("Parsed Date: " + userDate);
-        } catch (Exception e) {
-            System.out.println("Invalid date format. Please use dd-MM-yyyy.");
-        }
-
-        // Close the scanner
-        scanner.close();
+        Scanner sc = new Scanner(System.in);
+        String date = sc.nextLine();
+        DateTimeFormatter a =  DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate parsedDate = LocalDate.parse(date, a);
+        LocalDate updatedDate = parsedDate.minusMonths(1);
+        System.out.println(parsedDate);
+        System.out.println(updatedDate);
+        sc.close();
     }
 }
