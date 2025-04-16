271e2b3a12502b259ee43793a68e7631af0a718b
gitcrusher <aayushguptasoni@gmail.com>
2025-04-16 16:39:06 +0530

    commit

---
+++ Generics/upperboundedwildcard.java
@@ -0,0 +1,28 @@
+package Generics;
+class animal{
+    void eat(){
+        System.out.println("Animal eats");
+    }
+}
+
+class cat extends animal{
+    void eat(){
+        System.out.println("cat Eats");
+    }
+}
+
+class dog extends animal{
+    void eat(){
+        System.out.println("dog Eats");
+    }
+}
+
+class rat{
+    void eat(){
+        System.out.println("rat Eats");
+    }
+}
+
+public class upperboundedwildcard {
+    
+}
