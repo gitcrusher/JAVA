5e7a1de69f760f753ddc26a9b55f617c2ed338cb
gitcrusher <aayushguptasoni@gmail.com>
2025-04-21 11:36:34 +0530

    commit

---
+++ Queue/queueBasics.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class queueBasics {
+    public static void main(String[] args) {
+        Queue<Integer> que = new LinkedList<>();
+        que.add(100);
+        System.out.println( );
+    }
+}
