6234f37fa66731b6a90790c613b24883a8923832
gitcrusher <aayushguptasoni@gmail.com>
2025-04-25 11:49:17 +0530

    commit

---
+++ Tree/basic.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class basic {
+    public static void main(String[] args) {
+        TreeSet<Integer> tree= new TreeSet<>();
+        tree.add(150);
+        tree.add(80);
+        tree.add(10);
+        System.out.println(tree);
+    }
+}
