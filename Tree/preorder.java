17aa6019263a297d8dc3a2a596c96a3d62670165
gitcrusher <aayushguptasoni@gmail.com>
2025-04-27 15:55:28 +0530

    commit

---
+++ Tree/preorder.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+
+
+
+public class preorder {
+    public static void main(String[] args) {
+        TreeSet <String> tree = new TreeSet<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter rollno\n");
+        int a = sc.nextInt();
+        sc.nextLine();
+        String b = sc.nextLine();
+        tree.add(a+b);
+        System.out.println(tree);
+    }
+}
